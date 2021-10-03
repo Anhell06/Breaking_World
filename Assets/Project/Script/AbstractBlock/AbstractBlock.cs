05f414bf39dd4324568f4006d1deb0d77b4f23e0
Lesnik56 <lesnikrus5@yandex.ru>
2021-10-03 15:59:48 +0500

    AbstractBlock включая все элементы, но отредачен

--- Assets/Project/Script/AbstractBlock/AbstractBlock.cs
+++ Assets/Project/Script/AbstractBlock/AbstractBlock.cs
@@ -15,25 +15,103 @@ public class AbstractBlock : MonoBehaviour
     private ItemSO item;
     [SerializeField]
     private MoveFieldChanelSO mover;
-   
+    private Vector3 currentPosition;
+    private AbstractBlock ClashBlock;
+
     private void StartTouch(Vector2 vector)
     {
         if (isMovable)
         {
-            Vector3 ver3 = new Vector3(vector.x, vector.y, 0);
+
+            if (transform.position.x >= currentPosition.x + 1)
+            {
+                transform.position = currentPosition;
+            }
+            if (transform.position.z >= currentPosition.z + 1)
+            {
+                transform.position = currentPosition;
+            }
+            if (transform.position.x <= currentPosition.x - 1)
+            {
+                transform.position = currentPosition;
+            }
+            if (transform.position.z <= currentPosition.z - 1)
+            {
+                transform.position = currentPosition;
+            }
+            Vector3 ver3 = Vector2ToVector3(vector);
 
             transform.position += ver3;
         }
 
     }
-  
-    public void Start()
+
+    private static Vector3 Vector2ToVector3(Vector2 vector)
+    {
+        return new Vector3(vector.x, 0, vector.y);
+    }
+
+    private void Start()
     {
         mover.TouchForMoveFildeUpdate += StartTouch;
-
+        currentPosition = transform.position;
+        mover.TouchBeEnded += EndMove;
     }
-    public void OnDestroy()
+    private void OnDestroy()
     {
         mover.TouchForMoveFildeUpdate -= StartTouch;
+        mover.TouchBeEnded -= EndMove;
     }
+    private void EndMove(Vector2 vector)
+    {
+        if (isMovable)
+        {
+            Vector3 ver3 = Vector2ToVector3(vector);
+            transform.position += ver3;
+            currentPosition = transform.position;
+
+        }
+        if (ClashBlock != null)
+        {
+            Destroy();
+        }
+
+    }
+    public void Destroy()
+    {
+        if (isDestroyable)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public ItemSO GetItem()
+    {
+        if (item == null)
+        {
+            return null;
+        }
+        if (item.isUsable)
+        {
+            return item;
+        }
+        return null;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<AbstractBlock>())
+        {
+            ClashBlock = other.GetComponent<AbstractBlock>();
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        ClashBlock = null;
+    }
+    public void ChangeTypeThisBlock(AbstractBlock BlockChange)
+    {
+        var block = gameObject.AddComponent<AbstractBlock>();
+        block = BlockChange;
+    }
+
 }
